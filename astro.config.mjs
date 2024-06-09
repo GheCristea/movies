c2b93cdcd884bfecd1d21ac9d09c73e14d36938a
GheCristea <gheorghita.cristea@zoro.co.uk>
2024-06-09 23:26:33 +0100

    search suggest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,7 +14,5 @@ export default defineConfig({
   },
   integrations: [tailwind()],
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel()
 });
--- src/pages/api/search-suggestions.ts
+++ src/pages/api/search-suggestions.ts
@@ -2,16 +2,35 @@ import type { APIRoute } from 'astro';
 import axios from 'axios';
 import type { SearchResult } from '../../types/SearchResult';
 import type { SearchSuggestion } from '../../types/searchSuggestion';
+import zod from 'zod';
 
-export const post: APIRoute = async ({ request }) => {
+const SearchResultSchema: zod.ZodType<SearchResult[]> = zod.array(zod.object({
+    show: zod.object({
+        id: zod.number(),
+        name: zod.string(),
+        genres: zod.array(zod.string()),
+        image: zod.object({
+            medium: zod.string()
+        }).nullable()
+    })
+}))
+
+export const POST: APIRoute = async ({ request }) => {
     const formData = await request.formData()
     console.log('form data', formData);
     const query = formData.get('search')?.toString() || '';
 
     if (query) {
         console.log(query);
-        const { data } = await axios.get<SearchResult[]>(`https://api.tvmaze.com/search/shows?q=${query}`);
-        return new Response(JSON.stringify(getMappedSearchSuggestions(data)), {
+        const { data } = await axios.get<unknown>(`https://api.tvmaze.com/search/shows?q=${query}`);
+        const res = SearchResultSchema.safeParse(data);
+
+        if (!res.success) {
+            console.log(JSON.stringify(res.error));
+            throw new Error('Something went wrong');
+        }
+        
+        return new Response(JSON.stringify(getMappedSearchSuggestions(res.data)), {
             headers: { 'Content-Type': 'application/json' },
         });
     }
